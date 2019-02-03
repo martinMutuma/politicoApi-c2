3c2cc935ffba9ee40762971a9819a87ceabcc749
Martin Mutuma <smartimutuma@gmail.com>
2019-02-03 22:07:16 +0300

    Update Tests and Parties [finishes #163674566]

--- app/v1/views/parties.py
+++ app/v1/views/parties.py
@@ -9,15 +9,34 @@ partiesList = {}
 def post_party():
     """ Party data """
     data = get_data()
+
     validateRequired = Validate.required(fields=['name','hqAddress','logoUrl'], dataDict=data) 
-    if validateRequired == True:
-        party = create_party(data['name'], data['hqAddress'], data['logoUrl'])
-        returnPartydetails ={'name':party['name'], 'id':party['id']}
-        res = jsonify({"status":201, 'data':returnPartydetails})
-        return make_response(res, 201)
-    else:
-        res = jsonify({'status':400, 'error':validateRequired, 'data':[]})
+    if validateRequired['status'] == False:
+        res = jsonify({'status':400, 'error':validateRequired['message'], 'data':[]})
         return make_response(res, 400)
+
+
+    validateName = Validate.validate_name(data['name'])
+    if validateName['status'] == False:
+        res = jsonify({'status':400, 'error':validateName['message'], 'data':[]})
+        return make_response(res, 400)
+
+    validateAddressLen = Validate.validate_length(data['hqAddress'] , 5)
+    if validateAddressLen['status'] == False:
+        res = jsonify({'status':400, 'error':"hqAddress "+validateAddressLen['message'], 'data':[]})
+        return make_response(res, 400)
+
+    # validateLogoUrl = Validate.validate_url(data['logoUrl'])
+    # if validateLogoUrl['status'] == False:
+    #         res = jsonify({'status':400, 'error':"logoUrl "+validateLogoUrl['message'], 'data':[]})
+    #         return make_response(res, 400)
+
+    party = create_party(data['name'], data['hqAddress'], data['logoUrl'])
+    returnPartydetails ={'name':party['name'], 'id':party['id']}
+    res = jsonify({"status":201, 'data':returnPartydetails})
+    return make_response(res, 201)
+    
+       
 
 
 
@@ -25,7 +44,9 @@ def post_party():
     
 def create_party(name, hqAddress,logoUrl):
     """Central place to create party for uniformity"""
-    partyid = len(partiesList)-1
+    #validation 
+    
+    partyid = len(partiesList)+1
     newParty = dict(id=partyid, name=name ,hqAddress=hqAddress, logoUrl=logoUrl)
     partiesList[partyid] = newParty
     return newParty
