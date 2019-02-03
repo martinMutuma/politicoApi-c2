21d9300ee0e5bfa851b53c2ed3cdab900c09af33
Martin Mutuma <smartimutuma@gmail.com>
2019-02-03 14:42:04 +0300

    Add Basic flask App [finishes #163681829]

---
+++ app/routes.py
@@ -0,0 +1,19 @@
+"""app/routes.py """
+
+from app import polApp
+
+from flask import url_for, jsonify, make_response
+
+@polApp.route('/', methods=['GET'])
+def home():
+        
+    routes = []
+    for route in polApp.url_map.iter_rules():
+        routes.append(str(route))
+    res = dict(
+        data = routes,
+        status =200,
+    )
+    return make_response(jsonify(res), 200)
+       
+
