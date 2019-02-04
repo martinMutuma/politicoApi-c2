ecb150afb4cf94b5a1dd7d9877fed0fda0dda734
Martin Mutuma <smartimutuma@gmail.com>
2019-02-04 22:56:30 +0300

    [finishes #163674596] Add get all parties and tests

--- app/v1/__init__.py
+++ app/v1/__init__.py
@@ -3,11 +3,12 @@ from flask import Blueprint
 
 v1_app = Blueprint('api_v1', __name__)
 
-from app.v1.views.parties import post_party, get_party_details
+from app.v1.views.parties import post_party, get_party_details, get_all_parties
 
 #parties module urls
 v1_app.add_url_rule('/parties',view_func=post_party, methods=['POST'])
-v1_app.add_url_rule('/parties/<int:partyId>',view_func=get_party_details, methods=['get'])
+v1_app.add_url_rule('/parties/<int:partyId>',view_func=get_party_details, methods=['GET'])
+v1_app.add_url_rule('/parties/',view_func=get_all_parties, methods=['GET'])
 
 
  
